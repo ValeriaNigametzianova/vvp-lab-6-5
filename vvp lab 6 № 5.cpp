f95152e5251c13f371e210307ff95b19ae35bb85
Валерия <valeria103020@gmail.com>
2020-11-02 11:55:08 +0300

    Добавьте файлы проекта.

---
+++ vvp lab 6 № 5.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+int F(x)
+{
+	int t = pow(x - 3, 3);
+	int y = 4 * t * t - 7 * t + 2;
+}
+int main()
+{
+	int y, x, t;
+	printf("x = ");
+	scanf_s("%d", &x);
+	y = F(x);
+	printf("y = %d", y);
+	return 0;
+}
